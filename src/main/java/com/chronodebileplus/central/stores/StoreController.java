05c7d844c7af9fde58bedd20a3cc4fbfa7da5a62
clement-jny <clementjaunay@free.fr>
2025-04-10 23:03:15 +0200

    feat: commented implementation for StoreController

---
+++ src/main/java/com/chronodebileplus/central/stores/StoreController.java
@@ -0,0 +1,40 @@
+// package com.chronodebileplus.central.stores;
+
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.List;
+
+// @Configuration
+// @RestController
+// @RequestMapping("/central")
+// public class StoreController {
+//     // private final StoreService storeService;
+
+//     // @Value("${myStore.name}")
+//     // private String _myStoreName;
+//     // @Value("${myStore.endpoint}")
+//     // private String _myStoreEndpoint;
+
+//     // private Store _myStore = new Store(_myStoreName, _myStoreEndpoint);
+
+//     // public StoreController(StoreService _storeService) {
+//     //     this.storeService = _storeService;
+//     // }
+
+//     // @GetMapping()
+//     // public List<Store> retrieveStore() {
+//     //     return this.storeService.retrieveStores();
+//     // }
+
+//     // @PostMapping("/register")
+//     // public Store registerStore() {
+//     //     // Utilise la méthode centralisée pour vérifier l'existence du nom
+//     //     if (storeService.storeNameExists(_myStoreName)) {
+//     //         throw new IllegalArgumentException("Le nom du magasin est déjà utilisé: " + _myStoreName);
+//     //     }
+        
+//     //     return this.storeService.registerStore(_myStore);
+//     // }
+// }
