674e8667c714ca66c7c03f347d61249bef53c3d2
clement-jny <clementjaunay@free.fr>
2025-04-10 16:09:51 +0200

    feat: add RestClient for central

--- src/main/java/com/chronodebileplus/chrono/ChronoDebilePlusApplication.java
+++ src/main/java/com/chronodebileplus/chrono/ChronoDebilePlusApplication.java
@@ -1,13 +1,22 @@
 package com.chronodebileplus.chrono;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class ChronoDebilePlusApplication {
+	@Value("${central.endpoint}")
+	private String centralEndpoint;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChronoDebilePlusApplication.class, args);
 	}
 
+	@Bean
+	public RestClient restClient() {
+		return RestClient.create(this.centralEndpoint);
+	}
 }
