5f421b9ba9340507fa76082a56c1cb58ba234fba
clement-jny <clementjaunay@free.fr>
2025-04-10 15:36:54 +0200

    Initial commit

---
+++ src/main/java/com/chronodebileplus/chrono/ChronoDebilePlusApplication.java
@@ -0,0 +1,13 @@
+package com.chronodebileplus.chrono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoDebilePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoDebilePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronodebileplus/chrono/ChronoDebilePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronodebileplus.chrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoDebilePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
